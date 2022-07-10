6a69d1cc737c72b9f20e6b56f01113e289d0800f
Айрат Молотков <airat@promo-z.ru>
2022-07-10 20:32:42 +0400

    add database project

---
+++ SinglePage.Data/Models/VirtualServer.cs
@@ -0,0 +1,8 @@
+﻿namespace SinglePage.Data.Models;
+
+public class VirtualServer
+{
+    public int VirtualServerId { get; set; }
+    public DateTime CreateDateTime { get; set;}
+    public DateTime RemoveDateTime { get; set;}
+}
